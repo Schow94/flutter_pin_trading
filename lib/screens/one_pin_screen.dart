c8e9fee1b31e889cdc9e2415f7d4bd4f07457eb2
Schow94 <Schow94@gmail.com>
2020-09-08 14:47:12 -0700

    Fix one pin screen

--- lib/screens/one_pin_screen.dart
+++ lib/screens/one_pin_screen.dart
@@ -13,57 +13,124 @@ class OnePinScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Image.network(selectedPin.imageUrl),
-            Container(
-              margin: EdgeInsets.only(top: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  Flexible(
-                    child: Text(
-                      selectedPin.name,
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
-                  Text(
-                    '\$${selectedPin.price}',
-                    style: TextStyle(
-                      fontSize: 25,
-                      color: Colors.blue,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Column(
-              children: <Widget>[
-                // Want to left align descripton header
-                Align(
-                  child: Text('Description'),
-                ),
-                Text('${selectedPin.description}'),
-              ],
-            ),
-            Expanded(
+      body: ListView(
+        children: <Widget>[
+          Container(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
               child: Column(
                 children: <Widget>[
-                  Text('Details'),
-                  // Unable to show ListView unless I wrap it in a container w/
-                  // a fixed height
-                  // Expanded() is not working
-                  ListView.builder(
-                    physics: NeverScrollableScrollPhysics(),
-                    itemBuilder: (ctx, idx) => Text(selectedPin.details[idx]),
-                    itemCount: selectedPin.details.length,
+                  Image.network(selectedPin.imageUrl),
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(top: 20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            Flexible(
+                              child: Text(
+                                selectedPin.name,
+                                style: TextStyle(
+                                    fontSize: 25, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Text(
+                              '\$${selectedPin.price}',
+                              style: TextStyle(
+                                fontSize: 25,
+                                color: Colors.blue,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      ButtonBar(
+                        children: <Widget>[
+                          RaisedButton(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                            padding: EdgeInsets.all(10),
+                            onPressed: () {},
+                            child: Row(
+                              children: <Widget>[
+                                Text(
+                                  'Add to Wishlist',
+                                  style: TextStyle(fontSize: 18),
+                                ),
+                                Icon(Icons.favorite),
+                              ],
+                            ),
+                            color: Colors.pink,
+                          ),
+                          RaisedButton(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                            padding: EdgeInsets.all(10),
+                            onPressed: () {},
+                            child: Row(
+                              children: <Widget>[
+                                Text(
+                                  'Buy Now',
+                                  style: TextStyle(fontSize: 18),
+                                ),
+                                Icon(Icons.shopping_cart),
+                              ],
+                            ),
+                            color: Colors.blue,
+                          ),
+                        ],
+                      ),
+                      Divider(),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      // Want to left align descripton header
+                      Padding(
+                        padding: EdgeInsets.only(top: 10, bottom: 10),
+                        child: Text(
+                          'Description',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+
+                      Text('${selectedPin.description}'),
+                      Divider(),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(top: 10, bottom: 10),
+                        child: Text(
+                          'Details',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      // for (var detail in selectedPin.details) Text(detail),
+                      // Equivalent to ^^ the above
+                      ...selectedPin.details.map(
+                        (val) => Text('- ${val}'),
+                      ),
+                    ],
                   ),
                 ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
